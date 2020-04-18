c63605e78fc94fe3c236b186c6c283bb227ad5e0
Roman Kozlov <rkozlov95@mail.ru>
2020-04-19 01:19:53 +0300

    add twig, templates

--- Makefile
+++ Makefile
@@ -2,3 +2,5 @@ build-and-run:
 	docker-compose up -d --build
 run-mysql:
 	docker-compose exec db mysql -u root -p
+lint:
+	composer run-script phpcs -- --standard=PSR12 www
--- www/index.php
+++ www/index.php
@@ -2,23 +2,10 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-?>
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
-<link rel="stylesheet" href="bootstrap.min.css">
+$loader = new FilesystemLoader(__DIR__ . '/templates');
+$twig = new Environment($loader);
 
-<div class="container">
-  <h1 class="text-center">Simple authorization form</h1>
-  <form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-</div>
-
+echo $twig->render('index.html.twig');
